d03fe338a56fe42b87653f00c168f8592c925b1d
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2020-10-28 12:39:52 +0100

    Cambios en tests

---
+++ tests/test_leerdoubles.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    FILE* fp = fopen("salida_calculo", "rb");
+    double num;
+    int i = 0;
+    while (!feof(fp)) {
+        if (fread(&num, sizeof(double), 1, fp) == 1) {
+            printf("DOUBLE %d: %lf\n", i++, num);
+        }
+    }
+}
