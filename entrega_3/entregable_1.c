80b20948882ac289ead1a09ab4a8be55ac19c572
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2020-11-09 13:55:36 +0100

    Hecho el entregable 1

--- entrega_3/entregable_1.c
+++ entrega_3/entregable_1.c
@@ -1,3 +1,44 @@
-int main() {
-    
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int contador_sigint = 0; // Esta variable es el contador global del cuántas veces hemos recibido SIGINT. Tiene que ser global, porque no hay conexión entre el main y el gestor_sigint
+
+static void gestor_sigint(int signal); // La función que gestiona las interrupciones
+
+int main()
+{
+    if (signal(SIGINT, gestor_sigint) == SIG_ERR)
+    {
+        perror("Error al asignar la señal. Abortando.");
+        exit(EXIT_FAILURE);
+    }
+    for(;;);
+}
+
+static void gestor_sigint(int signal_id)
+{
+    char opcion;
+    if (contador_sigint >= 4)
+    {
+        printf("Deseas volver a la gestión por defecto de SIGINT? (s/N): ");
+        scanf(" %c", &opcion);
+        if (opcion == 's' || opcion == 'S')
+        {
+            if (signal(SIGINT, SIG_DFL) == SIG_ERR)
+            {
+                perror("Error al asignar la señal. Abortando.");
+                exit(EXIT_FAILURE);
+            }
+        } else { // Cualquier opción que no sea S
+            printf("Elegido: %c. Se mantiene el comportamiento actual.\n", opcion);
+        }
+    }
+    else
+    {
+        psignal(signal_id, "Se ha recibido la señal: ");
+        contador_sigint++;
+    }
 }
