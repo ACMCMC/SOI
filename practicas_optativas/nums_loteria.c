a9296dea193c32c28e3ef959f20a11117d938a1b
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2020-11-29 14:44:21 +0100

    Hecha la cola de procesos

--- practicas_optativas/nums_loteria.c
+++ practicas_optativas/nums_loteria.c
@@ -8,12 +8,27 @@
 
 double suma_doble_precision = 0;
 
-typedef struct num_lista {
+typedef struct num_lista
+{
     unsigned int num;
-    struct num_lista* sig;
+    struct num_lista *sig;
 } num_lista;
 
-num_lista* lista_enlazada_nums;
+num_lista *lista_enlazada_nums;
+
+typedef struct elem_cola_procesos
+{
+    pthread_t elem;
+    struct elem_cola_procesos *sig;
+} elem_cola_procesos;
+
+typedef struct cola_procesos {
+    elem_cola_procesos* principio;
+    elem_cola_procesos* final;
+    int num_elems;
+} cola_procesos;
+
+cola_procesos cola_proc;
 
 //Función privada que genera un número aleatorio entre inf y sup
 unsigned int gen_num_loteria()
@@ -22,11 +37,14 @@ unsigned int gen_num_loteria()
 }
 
 // Mira si el número ya ha sido generado antes
-int num_ya_generado(unsigned int num) {
+int num_ya_generado(unsigned int num)
+{
     num_lista elem_lista;
     elem_lista = *lista_enlazada_nums;
-    while (elem_lista.sig!=NULL) {
-        if (elem_lista.num==num) {
+    while (elem_lista.sig != NULL)
+    {
+        if (elem_lista.num == num)
+        {
             printf("Ya generado: %d\n", elem_lista.num);
             return 1;
         }
@@ -36,18 +54,57 @@ int num_ya_generado(unsigned int num) {
 }
 
 // Añade un número a la lista de números generados
-void anadir_lista(unsigned int num) {
-    num_lista* elem_lista = (num_lista*) malloc(sizeof(num_lista));
+void anadir_lista(unsigned int num)
+{
+    num_lista *elem_lista = (num_lista *)malloc(sizeof(num_lista));
     elem_lista->num = num;
     elem_lista->sig = lista_enlazada_nums;
     lista_enlazada_nums = elem_lista;
 }
 
-static void gestor_sigusr1() {
+// Devuelve TRUE si la cola de procesos está vacía
+int es_vacia_cola_procesos()
+{
+    return cola_proc.principio == NULL;
+}
+
+// Añade un proceso a la cola de procesos
+void anadir_cola_procesos(pthread_t proc)
+{
+    // Vamos a crear un nuevo elemento en la cola de procesos.
+
+    elem_cola_procesos* elem = (elem_cola_procesos*) malloc(sizeof(elem_cola_procesos)); // Reservamos memoria para un nuevo elemento de la cola
+    elem->sig = NULL; // El elemento lo vamos a meter al final, así que el siguiente a este no existe
+    elem->elem = proc; // El valor del elemento que vamos a meter en la cola será el identificador de proceso que le pasamos a esta función
+    if (!es_vacia_cola_procesos()) {
+        cola_proc.final->sig = elem;
+    } else { // Si la cola es vacía, entonces el primero elemento será este
+        cola_proc.principio = elem;
+    }
+    cola_proc.final = elem; // El final de la cola será este elemento
+    cola_proc.num_elems++; // Incrementamos el contador de elementos de la cola
+}
+
+// Devuelve el primer elemento de la cola de procesos, y lo elimina de la cola
+pthread_t primero_cola_procesos()
+{
+    pthread_t proc;
+    elem_cola_procesos* sig_elem;
+    proc = cola_proc.principio->elem;
+    sig_elem = cola_proc.principio->sig;
+    free(cola_proc.principio);
+    cola_proc.principio = sig_elem;
+    cola_proc.num_elems--;
+    return proc;
+}
+
+static void gestor_sigusr1()
+{
     // Liberamos la lista enlazada antes de salir
     num_lista *elemsig = lista_enlazada_nums;
     num_lista *elem;
-    while (elemsig!=NULL) {
+    while (elemsig != NULL)
+    {
         elem = elemsig;
         elemsig = elem->sig;
         free(elem);
@@ -58,41 +115,86 @@ static void gestor_sigusr1() {
     exit(EXIT_SUCCESS);
 }
 
-void* hilo_trabajador(void* term) {
+void *hilo_mostrar_sumas()
+{
+}
+
+void *hilo_contabilidad()
+{
+    pthread_t hilo;
+    while (1)
+    {
+        if (!es_vacia_cola_procesos())
+        {
+            hilo = primero_cola_procesos();
+            printf("Waiting to join thread: %p\n", hilo);
+            pthread_join(hilo, NULL);
+            printf("Joined thread: %p\n", hilo);
+        } else {
+            sched_yield();
+        }
+    }
+}
+
+void *hilo_trabajador(void *term)
+{
     unsigned int num_loteria;
     int terminacion = (int)(uintptr_t)term;
-    do {
+    do
+    {
         num_loteria = gen_num_loteria();
-    } while ((num_loteria%10)!=terminacion || num_ya_generado(num_loteria));
+    } while ((num_loteria % 10) != terminacion || num_ya_generado(num_loteria));
     anadir_lista(num_loteria);
     sleep(8);
     printf("Numero generado: %d\n", num_loteria);
 }
 
-int main() {
-    pthread_t* hilos = NULL;
+int main()
+{
+    pthread_t hilo_cont;
+    pthread_t hilo_sumas;
+    pthread_t hilo;
     int terminacion;
-    int num_hilos = 0;
+
+    cola_proc.num_elems = 0;
 
     // Inicializamos la lista
-    lista_enlazada_nums = (num_lista*) malloc(sizeof(num_lista));
-    lista_enlazada_nums->sig=NULL;
-    lista_enlazada_nums->num=-1;
+    lista_enlazada_nums = (num_lista *)malloc(sizeof(num_lista));
+    lista_enlazada_nums->sig = NULL;
+    lista_enlazada_nums->num = -1;
 
     srand((unsigned int)time(NULL)); // Semilla para generar números de lotería aleatoriamente
 
-    if(signal(SIGUSR1, gestor_sigusr1)==SIG_ERR) { // El proceso se termina con SIGUSR1
+    if (signal(SIGUSR1, gestor_sigusr1) == SIG_ERR)
+    { // El proceso se termina con SIGUSR1
         perror("Error en signal()");
     }
-    if(signal(SIGINT, SIG_IGN)==SIG_ERR) { // Ignoramos SIGINT, el proceso se termina con SIGUSR1
+    if (signal(SIGINT, SIG_IGN) == SIG_ERR)
+    { // Ignoramos SIGINT, el proceso se termina con SIGUSR1
         perror("Error en signal()");
     }
 
-    while (1) {
+    if (pthread_create(&hilo_cont, NULL, hilo_contabilidad, NULL))
+    {
+        perror("Error creando el hilo de contabilidad");
+    }
+
+    if (pthread_create(&hilo_sumas, NULL, hilo_mostrar_sumas, NULL))
+    {
+        perror("Error creando el hilo de mostrar sumas");
+    }
+
+    while (1)
+    {
         printf("Introduce una terminación: ");
         scanf(" %d", &terminacion);
-        hilos = (pthread_t *) realloc(hilos, sizeof(pthread_t)*(num_hilos+1));
-        pthread_create(hilos+num_hilos, NULL, hilo_trabajador, (void*)(uintptr_t)(terminacion%10));
-        num_hilos++;
+        if (pthread_create(&hilo, NULL, hilo_trabajador, (void *)(uintptr_t)(terminacion % 10)))
+        { // Creamos un hilo que calcule esa terminación
+            perror("Error creando el hilo");
+        }
+        else
+        {
+            anadir_cola_procesos(hilo);
+        }
     }
 }
